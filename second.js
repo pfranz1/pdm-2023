c1c32601640cae259fa1ccc60794e6369b60411e
pfranz1 <pfranz1@lsu.edu>
2023-01-19 16:00:43 -0600

    add example 2

--- second.js
+++ second.js
@@ -1,30 +1,49 @@
 const secondS = (p) => {
-    let x = 0;
     let canvasWidth, canvasHeight;
-    canvasWidth = canvasHeight = 100;
+    canvasWidth = canvasHeight = 250;
 
-    let overlap = 20;
+    let padding = 10;
+
+    let overlap = 12;
+
+    let radius;
   
     p.setup = function(){
       p.createCanvas(canvasWidth,canvasHeight);
+      p.colorMode("hsb")
 
       centerX = canvasHeight / 2;
       centerY = canvasWidth / 2;
 
+      radius = ((canvasHeight / 2 - padding) / 2);
+
       topCircleX = centerX;
-      topCircleY = centerY 
+      topCircleY = centerY - (radius-overlap);
 
+      leftCircleX = centerX - ((radius-overlap) * Math.cos(Math.PI / 6));
+      leftCircleY = centerY + ((radius-overlap) * Math.sin(Math.PI / 6));
+        
+    
+      rightCircleX = centerX + ((radius-overlap) * Math.cos(Math.PI / 6));
+      rightCircleY = centerY + ((radius-overlap) * Math.sin(Math.PI / 6));
 
+      radius = radius + overlap / 2
     };
   
     p.draw = function(){
-      p.background(0)
-      p.ellipse(x,p.height/2,20,20);
-  
-      x = x + 2;
-      if (x > canvasWidth){
-        x = 0;
-      }
+        p.background(255);
+        p.strokeWeight(0);
+
+        p.fill(0,755,100,0.5);
+        p.ellipse(topCircleX,topCircleY,radius*2,radius*2);
+
+        p.fill(250,75,100,0.5);
+        p.ellipse(leftCircleX,leftCircleY,radius*2,radius*2);
+
+        p.fill(120,75,100,0.5);
+        p.ellipse(rightCircleX,rightCircleY,radius*2,radius*2);
+
+        // p.ellipse(centerX,centerY,5,5)
     }
   }
   
