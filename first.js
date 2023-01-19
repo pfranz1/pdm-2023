24cde59f6d2d4af65bf845ffc049349b38954018
pfranz1 <pfranz1@lsu.edu>
2023-01-19 14:03:09 -0600

    add circle

--- first.js
+++ first.js
@@ -16,21 +16,30 @@
 //   }
 // }
 const firstS = (p) => {
-  let x = 0;
-  let canvasWidth = 1000;
+  // let x = 0;
+  let canvasWidth = 500;
+  let canvasHeight = canvasWidth / 2;
+  let padding = canvasWidth * 1 / 50;
+
+
+  let circleRadius;
+  
 
   p.setup = function(){
-    p.createCanvas(canvasWidth,100);
+    p.createCanvas(canvasWidth,canvasHeight);
+    
+    let maxWidth = canvasWidth * 0.25;
+    let maxHeight = canvasHeight * 0.5;
+
+    // Store circle radius, ensureing that it fits
+    circleRadius = Math.min(maxWidth,maxHeight) - padding;
   };
 
   p.draw = function(){
-    p.background(0)
-    p.ellipse(x,p.height/2,20,20);
+    p.background(0,255,0);
 
-    x = x + 1;
-    if (x > canvasWidth){
-      x = 0;
-    }
+    p.ellipse(canvasWidth * 0.25,canvasHeight * 0.5,circleRadius * 2,circleRadius * 2);
+
   }
 }
 
