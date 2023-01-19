08565ca31e9ac16721eded92a329aba5199d3cd2
pfranz1 <pfranz1@lsu.edu>
2023-01-19 14:17:32 -0600

    complete example 1

--- first.js
+++ first.js
@@ -1,30 +1,11 @@
-// let x = 0;
-// let canvasWidth = 1000;
-
-// function setup() {
-//   var canvas = createCanvas(canvasWidth, 100);
-//   canvas.parent('first')
-//   background(100);
-// }
-
-// function draw() {
-//   ellipse(x,height/2,20,20);
-  
-//   x = x + 1;
-//   if (x > canvasWidth){
-//     x = 0;
-//   }
-// }
 const firstS = (p) => {
   // let x = 0;
   let canvasWidth = 500;
   let canvasHeight = canvasWidth / 2;
-  let padding = canvasWidth * 1 / 50;
-
+  let padding = canvasWidth * 1 / 25;
 
   let circleRadius;
   
-
   p.setup = function(){
     p.createCanvas(canvasWidth,canvasHeight);
     
@@ -39,6 +20,7 @@ const firstS = (p) => {
     p.background(0,255,0);
 
     p.ellipse(canvasWidth * 0.25,canvasHeight * 0.5,circleRadius * 2,circleRadius * 2);
+    p.rect(canvasWidth*0.75 - circleRadius, padding,circleRadius*2,circleRadius * 2);
 
   }
 }
