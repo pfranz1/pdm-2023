fa743a5b4a40a57e9f6e9bc4414db9669003c6fe
pfranz1 <pfranz1@lsu.edu>
2023-01-29 14:43:09 -0600

    add onTap call back and call it when colorPicker tapped

--- prog2/paintCanvas.js
+++ prog2/paintCanvas.js
@@ -29,11 +29,25 @@ class ColorPicker{
 
     xPos;
     yPos;
+
+    isInside(tapX,tapY){
+        let inX = tapX >= this.xPos && tapX <= this.xPos + ColorPicker.size;
+        let inY = tapY >= this.yPos && tapY <= this.yPos + ColorPicker.size;
+
+        return inX && inY;
+    }
+
+    checkTap(tapX,tapY){
+        if (this.isInside(tapX,tapY)){
+            this.onTap();
+        }
+    }
     
-    constructor(color, xPos,yPos){
+    constructor(color, xPos,yPos, onTap){
         this.color = color;
         this.xPos = xPos;
         this.yPos = yPos;
+        this.onTap = onTap;
     }
 
     doDraw(){
@@ -46,17 +60,29 @@ var myColorPickers;
 
 var availableColors = [new ColorDefinition(0,100,100),new ColorDefinition(30,100,100),new ColorDefinition(60,100,100),new ColorDefinition(90,100,100)]
 
+var currentlySelected = new ColorDefinition(0,0,0);
+var selectedIndex = -1;
+
 function setup() {
     createCanvas(canvasHeight, canvasWidth);
     colorMode('hsb');
 
-    myColorPickers = availableColors.map((color,index,_)=>{return new ColorPicker(color,0,index*ColorPicker.size)});
+    myColorPickers = availableColors.map((color,index,_)=>{
+            let onTap = () => {currentlySelected = color; selectedIndex=index;};
+            return new ColorPicker(color,0,index*ColorPicker.size,onTap);
+        });
+
     print(myColorPickers);
      
-  }
+}
+
+function mouseClicked(){
+    myColorPickers.map((colorPicker,_,__)=>{ colorPicker.checkTap(mouseX,mouseY)});
+    print(selectedIndex);
+}
   
-  function draw() {
+function draw() {
     background(220,123,214);
     ellipse(50,50,80,80);
     myColorPickers.map((colorPicker,_,__)=>{ colorPicker.doDraw()});
-  }
+}
