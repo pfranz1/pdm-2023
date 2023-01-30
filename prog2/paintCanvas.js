2406dc73ecd851c836b941b5ac9d3457e1208991
pfranz1 <pfranz1@lsu.edu>
2023-01-29 19:49:32 -0600

    add effect to show a color is selected

--- prog2/paintCanvas.js
+++ prog2/paintCanvas.js
@@ -43,16 +43,22 @@ class ColorPicker{
         }
     }
     
-    constructor(color, xPos,yPos, onTap){
+    constructor(color, xPos,yPos, onTap, isSelected){
         this.color = color;
         this.xPos = xPos;
         this.yPos = yPos;
         this.onTap = onTap;
+        this.isSelected = isSelected;
     }
 
     doDraw(){
+        push();
         this.color.doSetFill();
+        if(this.isSelected){
+            strokeWeight(padding / 2)
+        }
         square(this.xPos,this.yPos,ColorPicker.size);
+        pop();
     }
 }
 
@@ -63,26 +69,39 @@ var availableColors = [new ColorDefinition(0,100,100),new ColorDefinition(30,100
 var currentlySelected = new ColorDefinition(0,0,0);
 var selectedIndex = -1;
 
+var padding = 5;
+
+
 function setup() {
     createCanvas(canvasHeight, canvasWidth);
     colorMode('hsb');
 
+
     myColorPickers = availableColors.map((color,index,_)=>{
             let onTap = () => {currentlySelected = color; selectedIndex=index;};
-            return new ColorPicker(color,0,index*ColorPicker.size,onTap);
+            return new ColorPicker(color,padding,index*ColorPicker.size + padding*(index+1),onTap,index == selectedIndex);
         });
 
-    print(myColorPickers);
+    // print(myColorPickers);
      
 }
 
 function mouseClicked(){
+    let cachedSelectedIndex = selectedIndex;
+    
     myColorPickers.map((colorPicker,_,__)=>{ colorPicker.checkTap(mouseX,mouseY)});
-    print(selectedIndex);
+    
+    if (cachedSelectedIndex != selectedIndex){
+        myColorPickers = availableColors.map((color,index,_)=>{
+            let onTap = () => {currentlySelected = color; selectedIndex=index;};
+            return new ColorPicker(color,padding,index*ColorPicker.size + padding*(index+1),onTap,index == selectedIndex);
+        });
+        // print(myColorPickers);
+    }
+    // print(selectedIndex);
 }
   
 function draw() {
     background(220,123,214);
-    ellipse(50,50,80,80);
     myColorPickers.map((colorPicker,_,__)=>{ colorPicker.doDraw()});
 }
