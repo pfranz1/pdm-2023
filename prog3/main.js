6f84a841991634d4cbc7824f053eb6864e97ed60
Peter Franz <pfranz1@lsu.edu>
2023-02-02 14:05:39 -0600

    add movement

--- prog3/main.js
+++ prog3/main.js
@@ -8,6 +8,11 @@ let spriteHeight = 80;
 let animationLenght = 9;
 let currentFrame = 0;
 
+let xLocation = 200;
+let xDirection = 1;
+
+let isMoving = 0;
+
 
 
 // Index of sprite on sprite sheet
@@ -28,12 +33,33 @@ function draw(){
 
     u = currentFrame % animationLenght;
     
+    translate(xLocation,200);
+    scale(xDirection,1);
 
-    image(spriteSheet,200,200,80,80,u*spriteWidth,v*spriteHeight,spriteWidth,spriteHeight);
+    image(spriteSheet,0,0,80,80,u*spriteWidth,v*spriteHeight,spriteWidth,spriteHeight);
     if(frameCount % 6 == 0){
         currentFrame++;
     }
-    
 
+    xLocation += isMoving;
+
+}
+
+function keyPressed(){
+    if (keyCode === RIGHT_ARROW){
+        isMoving = 1;
+        xDirection = 1;
+    } 
+    else if (keyCode === LEFT_ARROW){
+        isMoving = -1;
+        xDirection = -1;
+    } 
+}
+
+
+function keyReleased(){
+    if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW){
+        isMoving = 0;
+    } 
 
 }
