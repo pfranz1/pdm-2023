f08a8ac6961cb1c33d294d03afa747d54eccd918
pfranz1 <pfranz1@lsu.edu>
2023-02-28 16:18:46 -0600

    add control siders for depth and feedback

--- prog6/js/sketch.js
+++ prog6/js/sketch.js
@@ -45,13 +45,22 @@ function setup() {
   };
   
   instrument.set(synthJSON);
+
+
+
+  var envelope = new Tone.Envelope({
+		attack: 0.1,
+		decay: 0.2,
+		sustain: 0.5,
+		release: 0.8,
+	})
   
   var effect1, effect2, effect3;
   
   // create effects
   var effect1 = new Tone.Chorus();
   effect1JSON = {
-    "frequency": 250,
+    "frequency": 1000000,
     "delayTime": 24,
     "type": "triangle",
     "depth": 20,
@@ -60,6 +69,20 @@ function setup() {
       "wet": 0.5
   };
   effect1.set(effect1JSON);
+
+
+  dSlider = createSlider(0., 100., 1, 0.05);
+  dSlider.position((width / 2) + 35, 125);
+  dSlider.mouseReleased( () => {
+    effect1.depth = dSlider.value();
+  })
+
+  fSlider = createSlider(0., 1., 0.5, 0.05);
+  fSlider.position((width / 2) + 35, 200);
+
+  fSlider.mouseReleased( () => {
+    effect1.feedback.value = fSlider.value();
+  })
   
   
   // make connections
@@ -77,5 +100,6 @@ function draw() {
 
 function keyPressed(){
   let whatNote = notes[key]
+  
   instrument.triggerAttackRelease(whatNote, "8n");
 }
