3d805ff665435f3a941e34336cc0b38bf8f479e8
pfranz1 <pfranz1@lsu.edu>
2023-02-28 16:01:21 -0600

    add synth sound

--- prog6/js/sketch.js
+++ prog6/js/sketch.js
@@ -1,7 +1,67 @@
+var instrument = new Tone.MonoSynth();
+
+
 function setup() {
+  // create synth
+  var synthJSON = {
+      "oscillator": {
+          "type": "fmsquare5",
+      "modulationType" : "triangle",
+          "modulationIndex" : 2,
+          "harmonicity" : 0.501
+      },
+      "filter": {
+          "Q": 1,
+          "type": "lowpass",
+          "rolloff": -24
+      },
+      "envelope": {
+          "attack": 0.01,
+          "decay": 0.1,
+          "sustain": 0.4,
+          "release": 2
+      },
+      "filterEnvelope": {
+          "attack": 0.01,
+          "decay": 0.1,
+          "sustain": 0.8,
+          "release": 1.5,
+          "baseFrequency": 50,
+          "octaves": 4.4
+      }
+  };
+  
+  instrument.set(synthJSON);
+  
+  var effect1, effect2, effect3;
+  
+  // create effects
+  var effect1 = new Tone.Chorus();
+  effect1JSON = {
+    "frequency": 16,
+    "delayTime": 16,
+    "type": "triangle",
+    "depth": 1,
+    "feedback": 0.1,
+    "spread": 120,
+      "wet": 0.5
+  };
+  effect1.set(effect1JSON);
+  
+  
+  // make connections
+  instrument.connect(effect1);
+  effect1.toDestination();
+
+  instrument.triggerAttackRelease("C4", "8n");
+
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+}
+
+function keyPressed(){
+  instrument.triggerAttackRelease("C4", "8n");
 }
