04c7d105197062cecc1ea9a3c323343e3b4ef0a7
pfranz1 <pfranz1@lsu.edu>
2023-03-11 22:20:29 -0600

    add chords

--- tone-playground/src/main.js
+++ tone-playground/src/main.js
@@ -7,6 +7,16 @@ const AMinorScale = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 let melody = addOctaveNumbers(AMinorScale,4);
 
+const IChord = constructMajorChord(AMinorScale, 4, 'A3');
+// Output: ['A3', 'C4', 'E4']
+const VChord = constructMajorChord(AMinorScale, 4, 'E4');
+// Output: ['E4', 'G4', 'B4']
+const VIChord = constructMajorChord(AMinorScale, 3, 'F3');
+// Output: ['F3', 'A3', 'C3']
+const IVChord = constructMajorChord(AMinorScale, 3, 'D3');
+
+melody = IVChord;
+
 function setup() {
 
   synth.toDestination();
@@ -23,7 +33,7 @@ function setup() {
     synth.triggerAttackRelease(note,0.5);
   },melody,'4n');
 
-  Tone.Transport.bpm.value = 80;
+  Tone.Transport.bpm.value = 150;
 
   Tone.Transport.start();
 
@@ -48,6 +58,30 @@ function addOctaveNumbers (scale, octaveNumber){
     const noteOctaveNumber = scale.indexOf(note) < firstOctaveNoteIndex ? octaveNumber - 1 : octaveNumber;
     return `${note}${noteOctaveNumber}`
   });
+}
+
+function constructMajorChord (scale, octave, rootNote) {
+  const scaleWithOctave = addOctaveNumbers(scale, octave);
+
+  const getNextChordNote = (note, nextNoteNumber) => {
+    const nextNoteInScaleIndex = scaleWithOctave.indexOf(note) + nextNoteNumber - 1;
+    let nextNote;
+    if (typeof(scaleWithOctave[nextNoteInScaleIndex]) !== 'undefined') {
+      nextNote = scaleWithOctave[nextNoteInScaleIndex];
+    } else {
+      nextNote = scaleWithOctave[nextNoteInScaleIndex - 7];
+      const updatedOctave = parseInt(nextNote.slice(1)) + 1;
+      nextNote = `${nextNote.slice(0,1)}${updatedOctave}`;
+    }
+
+    return nextNote;
+  }
+
+  const thirdNote = getNextChordNote(rootNote, 3)
+  const fifthNote = getNextChordNote(rootNote, 5)
+  const chord = [rootNote, thirdNote, fifthNote]
+
+  return chord
 }
 
 
