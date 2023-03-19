fcc68f2535c9edec17b12bd0d3a647e1c99bca35
pfranz1 <pfranz1@lsu.edu>
2023-03-18 20:08:01 -0500

    recreate bug squish in new folder

---
+++ prog8/main.js
@@ -0,0 +1,154 @@
+let mainGuySpriteSheet;
+let walkers;
+let bg;
+
+let canvasWidth = 500;
+let canvasHeight = 900;
+
+let padding = 50;
+
+let ripple;
+
+let gameScore = 0;
+let topScore = gameScore;
+
+let startTime = 30;
+let timeRemaining = startTime;
+
+
+function incScore(){
+    gameScore++;
+    print("Game score:", gameScore);
+}
+
+function preload(){
+    bugWalking = loadImage("./assets/Water-Skipper.png");
+    bugTwitch = loadImage("./assets/dead-skipper.png");
+
+    spawnBugs();
+    ripple = new Ripple(-100,-100,0,10,0);
+}
+
+
+function spawnBugs(){
+    // BugSprite(spiteSheet, tilingWidth, tilingHeight, numFramesInAnimation, drawingWidth, drawingHeight, xPos, yPos)
+    let numBugs = 30;
+    walkers = [];
+
+    
+    for (let i = 0; i < numBugs; i++) {
+        walkers.push(new BugSprite(bugWalking,bugTwitch,32,32,9,3,75,75,random(padding,canvasWidth - padding),random(padding,canvasHeight - padding), incScore ));
+    }
+}
+
+
+
+
+function setup(){
+    createCanvas(canvasWidth,canvasHeight);
+    imageMode(CENTER);
+    colorMode('hsb');
+}
+
+
+function endGame(){
+    timeRemaining = -1;
+    print("end game");
+    topScore = max(gameScore, topScore);
+    drawScoreScreen();
+    // spawnBugs();
+}
+
+function startGame(){
+    gameScore = 0;
+    timeRemaining = startTime;
+    spawnBugs();
+}
+
+function draw(){
+
+    if(timeRemaining == -1){
+        // drawScoreScreen();
+    } else {
+        background(236,70,100);
+
+
+        timeRemaining -= deltaTime / 1000;
+        if (timeRemaining < 0) {
+            endGame();
+        } else {
+            drawGame();
+        }
+    }
+}
+
+
+function drawGame(){
+    // print('draw ripple');
+    ripple.draw();
+
+    // Want the ripple not to cover text, but want bugs to crawl on text
+    textSize(50)
+    fill(200,0,100);
+    text("Score: " + gameScore, 10,padding);
+
+    text("Time: " + ceil(timeRemaining),width-200,padding);
+    
+
+    walkers.forEach( function (item,index){
+        item.draw();
+    });
+}
+
+function drawScoreScreen(){
+    background(236,70,100);
+
+    push();
+
+    let textStartY = canvasHeight * 0.30; 
+
+    textAlign('center');
+
+    textSize(48);
+    text("Game Over!", canvasWidth / 2, textStartY);
+
+    textSize(32);
+    text("Score: " + gameScore, (canvasWidth / 2), (textStartY)+40)
+
+
+    textSize(32);
+    text("Top: " + topScore, (canvasWidth / 2), (textStartY)+80)
+
+
+    button = createButton('Again!');
+    button.position((canvasWidth / 2) - 75, (textStartY) + 240 );
+    button.size(150);
+    button.mousePressed( ()=>  {button.remove(); startGame();});
+
+
+    pop();
+}
+
+function mouseReleased(){
+    //TODO: read from bugs if a tap occurred and dont do ripple if so
+    walkers.forEach( function (item,index){
+        item.tapOccurred(mouseX,mouseY);
+    });
+
+    // print("new ripple created at ", mouseX,mouseY);
+    ripple = new Ripple(mouseX,mouseY,50,150,50);
+
+}
+
+function keyPressed(){
+    // walkers.forEach( function (item,index){
+    //     item.keyPressed();
+    // });
+}
+
+
+function keyReleased(){
+    // walkers.forEach( function (item,index){
+    //     item.keyReleased();
+    // });
+}
---
+++ prog8/ripple.js
@@ -0,0 +1,73 @@
+class Ripple{
+
+    constructor(xPos,yPos, startRadius,endRadius, timeToLive){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.startRadius = startRadius;
+        this.endRadius = endRadius;
+        this.timeToLive = timeToLive;
+        this.life = timeToLive;
+
+    
+    }
+
+    
+
+    draw(){
+        if(this.life < 0) return;
+
+        this.life -= 1;
+        // print(this.life);
+        // print(this.timeToLive);
+
+        push();
+        //TODO: Put into some function to turn into non-linear curve
+        let driverValue = (this.life / this.timeToLive)**4;
+        // print("driverVal",driverValue);
+
+
+
+        let radius = (1 - driverValue) * this.endRadius + this.startRadius * driverValue;
+
+        stroke(0,0,0,0);
+
+        // Outer Rings
+        // fill(color(200,60,100));
+        fill(color(236,60,100));
+
+        // print("radius",radius);
+        ellipse(this.xPos,this.yPos, radius, radius);
+
+        // fill(color(200,80,100));
+        fill(color(236,70,100));
+
+        ellipse(this.xPos,this.yPos, radius - 10, radius- 10);
+
+        // Trailing rings
+
+        // fill(color(200,60,100));
+        fill(color(236,60,100));
+;
+        ellipse(this.xPos,this.yPos, max(20,radius - 25 - (radius * driverValue)));
+
+        // fill(color(200,80,100));
+        fill(color(236,70,100));
+
+        ellipse(this.xPos,this.yPos, max(radius - 35 -  (radius * driverValue), 20));
+
+
+        // // Central dot
+
+        let rippleColor = color(236,60,60);
+        rippleColor.setAlpha(driverValue -0.2)
+        
+        fill(rippleColor);
+
+        ellipse(this.xPos,this.yPos, radius - (radius * driverValue));
+
+
+
+        // ellipse(100,100,100,100);
+        pop();
+    };
+}
