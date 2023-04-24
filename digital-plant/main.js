e6cc0c81c3b78d6ac0d81cbd58c2d1590ba0365f
pfranz1 <pfranz1@lsu.edu>
2023-04-24 14:57:57 -0500

    init plant project

---
+++ digital-plant/main.js
@@ -0,0 +1,119 @@
+let mainGuySpriteSheet;
+let walkers;
+let bg;
+
+let canvasWidth = 1000;
+let canvasHeight = 500;
+
+let padding = 50;
+
+
+function preload(){
+    mainGuySpriteSheet = loadImage("./assets/SpelunkyGuy.png");
+    blueGuySpriteSheet = loadImage("./assets/BlueGuy.png");
+    robotSpriteSheet = loadImage("./assets/robot.png");
+
+    
+    // WalkingSprite(spiteSheet, tilingWidth, tilingHeight, numFramesInAnimation, drawingWidth, drawingHeight, xPos, yPos)
+    walkers = [ new WalkingSprite(mainGuySpriteSheet,80,80,9,80,80,canvasWidth/2,padding),
+                new WalkingSprite(blueGuySpriteSheet,80,80,9,80,80,canvasWidth/2,padding + (canvasHeight - (padding * 2)) / 2),
+                new WalkingSprite(robotSpriteSheet,80,80,9,80,80,canvasWidth/2,canvasHeight - padding),]
+}
+
+
+function setup(){
+    createCanvas(canvasWidth,canvasHeight);
+    imageMode(CENTER);
+    colorMode('hsb');
+}
+
+function draw(){
+    background(200,100,100);
+
+    walkers.forEach( function (item,index){
+        item.draw();
+    });
+
+}
+
+function keyPressed(){
+    walkers.forEach( function (item,index){
+        item.keyPressed();
+    });
+}
+
+
+function keyReleased(){
+    walkers.forEach( function (item,index){
+        item.keyReleased();
+    });
+}
+
+class WalkingSprite{
+    
+    constructor(spriteSheet, tileWidth, tileHeight, numAnimationFrames, height, width,  xPos,yPos,  ){
+        this.spriteSheet = spriteSheet;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.numAnimationFrames = numAnimationFrames;
+        this.height = height;
+        this.width = width;
+        this.xPos = xPos;
+        this.yPos = yPos;
+
+        this.currentFrame = 0;
+
+        // xDirection 1 means that the sprite is facing the right, -1 = left
+        this.xDirection = 1;
+
+        this.tileRowIterator = 0;
+        this.tileColumnIterator = 0;
+
+        this.moveSpeed = 0;
+    }
+
+    draw(){
+
+        this.tileColumnIterator = this.currentFrame % this.numAnimationFrames;
+        
+        push();
+        // Translating so that (0,0) corresponds to the sprites top left
+        translate(this.xPos,this.yPos);
+        
+        // Scaling to flip sprite if xDirection == -1
+        scale(this.xDirection,1);
+        
+        // Start at location 0 0 because of the translate  
+        image(this.spriteSheet,0,0,this.height,this.width,this.tileColumnIterator*this.tileWidth,this.tileRowIterator*this.tileHeight,this.tileWidth, this.tileHeight);
+        pop();
+        
+        // Only update frame every 6 p5 draw ticks and only if character moving
+        if(frameCount % 6 == 0 && this.moveSpeed != 0){
+            this.currentFrame++;
+        }
+        
+        // Change xPos by the move speed of the walker
+        this.xPos += this.moveSpeed;
+    }
+
+    keyPressed(){
+        if (keyCode === RIGHT_ARROW){
+            this.moveSpeed = 2;
+            this.xDirection = 1;
+            this.currentFrame = 1;
+        } 
+        else if (keyCode === LEFT_ARROW){
+            this.moveSpeed = -2;
+            this.xDirection = -1;
+            this.currentFrame = 1;
+        } 
+    }
+
+    
+    keyReleased(){
+        if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW){
+            this.moveSpeed = 0;
+            this.currentFrame = 0;
+        } 
+    }
+}
