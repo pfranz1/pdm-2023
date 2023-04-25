c895eda23a374bd66f6a065581556c510d806f5c
pfranz1 <pfranz1@lsu.edu>
2023-04-24 19:29:22 -0500

    allow for mutliple leaves to be drawn

--- digital-plant/main.js
+++ digital-plant/main.js
@@ -3,7 +3,7 @@ let walkers;
 let bg;
 
 let canvasWidth = 1000;
-let canvasHeight = 500;
+let canvasHeight = 1000;
 
 let padding = 50;
 
@@ -13,12 +13,16 @@ let leaf;
 
 let myFont;
 
+let myLeafRots = [ new RotationStruct(0, 0 ,0 ), new RotationStruct(45,0,0), new RotationStruct(30,0,0), new RotationStruct(0,-45,0)];
+
+let leaves = [];
+
 
 function preload(){
     myFont = loadFont('assets/NexaText-Trial-Regular.ttf');
 
     leafSprite = loadImage("./assets/leaf.png") ;
-    leaf = new Leaf(leafSprite,32,32,100,100,0,0);  
+    leaf = new Leaf(leafSprite,32,32,100,100,0,0, new RotationStruct(0,0,0));  
 }
 
 
@@ -34,41 +38,72 @@ function setup(){
     angleMode(DEGREES);
 
     createRotationSliders();
+
+    createLeavesFromRots();
+}
+
+let leafSize = 100;
+
+function createLeavesFromRots(){
+    let startY = canvasHeight / 2 * -1 + leafSize;
+    let startX = canvasWidth  / 2 * -1 + leafSize
+    let padding = 170;
+
+    myLeafRots.forEach((rotStruct,index,_)=>{
+        //spriteSheet, tileWidth, tileHeight, height,width,xPos,yPos, rotationStruct){
+        leaves.push(new Leaf(leafSprite,32,32,leafSize,leafSize,startX,startY,rotStruct));
+        startY += 100;
+
+        if(index % 8 == 0 && index != 0){
+            startX += leafSize + padding;
+        }
+    });
 }
 
 function draw(){
+
+
     background(200,100,100);
     leaf.draw();
+
+    leaves.forEach((value,index,_)=>{
+        value.draw();
+    })
 
     textSize(32);
     text('XROT:' + xRotSlider.value(),250,-100);
     text('YROT:' + yRotSlider.value(),250,0);
     text('ZROT:' + zRotSlider.value(),250,100);
 
+
+    fill(100,100,100)
+    circle(0,0,10)
+
 }
+
 
 function createRotationSliders(){
     
-    xRotSlider = createSlider(0, 360, 0, 1);
+    xRotSlider = createSlider(0, 90, 0, 1);
     xRotSlider.position(0,canvasHeight * 1.5);
   
-    // xRotSlider.mouseReleased( () => {
-    //     leaf.xRot = xRotSlider.value();
-    // })
+    xRotSlider.mouseReleased( () => {
+        leaf.xRot = xRotSlider.value();
+    })
 
-    yRotSlider = createSlider(0, 360, 0, 1);
+    yRotSlider = createSlider(-90, 90, 0, 1);
     yRotSlider.position(150,canvasHeight * 1.5);
   
-    // yRotSlider.mouseReleased( () => {
-    //     leaf.yRot = yRotSlider.value();
-    // })
+    yRotSlider.mouseReleased( () => {
+        leaf.yRot = yRotSlider.value();
+    })
 
-    zRotSlider = createSlider(0, 360, 0, 1);
+    zRotSlider = createSlider(-90,90, 0, 1);
     zRotSlider.position(300,canvasHeight * 1.5);
   
-    // zRotSlider.mouseReleased( () => {
-    //     leaf.zRot = zRotSlider.value();
-    // })
+    zRotSlider.mouseReleased( () => {
+        leaf.zRot = zRotSlider.value();
+    })
 
 }
 
@@ -84,35 +119,50 @@ function keyReleased(){
 }
 
 class Leaf {
-    constructor(spriteSheet, tileWidth, tileHeight, height,width,xPos,yPos){
+    constructor(spriteSheet, tileWidth, tileHeight, myHeight,myWidth,xPos,yPos, rotationStruct){
         this.spriteSheet = spriteSheet;
         this.tileWidth = tileWidth;
         this.tileHeight = tileHeight;
-        this.height = height;
-        this.width = width;
+        this.height = myHeight;
+        this.width = myWidth;
         this.xPos = xPos;
         this.yPos = yPos;
 
-        this.xRot = 0;
-        this.yRot = 0;
-        this.zRot = 0;
+        this.xRot = rotationStruct.xRot;
+        this.yRot = rotationStruct.yRot;
+        this.zRot = rotationStruct.zRot;
 
     }
 
     draw(){
         push();
 
+        // rotateX(this.xRot);
+
+        // translate(500,20);
         translate(this.xPos,this.yPos);
 
-        rotateX(xRotSlider.value());
-        rotateY(yRotSlider.value());
-        rotateZ(zRotSlider.value());
+        rotateX(this.xRot);
+        rotateY(this.yRot);
+        rotateZ(this.zRot);
 
-        translate(0, (this.height / 2) * 0.90);
 
-        // console.log(this.xRot, this.yRot,this.zRot);
+        image(this.spriteSheet, 0,0,this.width,this.height,0,0,this.tileWidth,this.tileHeight);
+        
 
-        image(this.spriteSheet, 0,0,this.height,this.width,0,0,this.tileWidth,this.tileHeight);
+        // rotateX(this.xRot);
+        // rotateY(this.yRot);
+        // rotateZ(this.zRot);
+
+
+        // translate(this.xPos,this.yPos);
+
+        // // square(this.xPos - this.width / 2,this.yPos, this.height);
+
+        // translate(0, (this.height / 2) * 0.90);
+
+        // // console.log(this.xRot, this.yRot,this.zRot);
+
 
         pop();
     }
